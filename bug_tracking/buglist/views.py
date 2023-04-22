c1f1890d295dda99274fbb5df6a78e06de4b2bf4
Yerko <bryanreyesavila@outlook.cl>
2023-04-22 19:43:50 -0400

    Modificaciones a los settins y views

--- bug_tracking/buglist/views.py
+++ bug_tracking/buglist/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from django.shortcuts import redirect, reverse
 from .models import Bug
+from database.models import Bug
 
 
 # Create your views here.
 
 def index(request):
-    latest_bug_list = Bug.objects.order_by("fecha")[:20]
+    latest_bug_list = Bug.objects.order_by("id_bug")[:20]
     context = {"latest_bug_list": latest_bug_list}
     return render(request, "buglist/index.html", context)
 
