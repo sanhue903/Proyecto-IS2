a8f8bba4a1cc6f7976435cd38741c4c89a6974f7
Yerko <bryanreyesavila@outlook.cl>
2023-04-22 19:33:32 -0400

    Página semifuncionando

--- bug_tracking/bug_tracking/settings.py
+++ bug_tracking/bug_tracking/settings.py
@@ -107,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-cl'
 
 TIME_ZONE = 'UTC'
 
@@ -122,7 +122,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    BASE_DIR / "buglist" / "static",
+    BASE_DIR / "home" / "static",
 ]
 
 # Default primary key field type
