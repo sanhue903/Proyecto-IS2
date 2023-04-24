13527bcc05116130316b8895ea299d461e71b7e9
Sebastián Sanhueza <s.sanhueza903@gmail.com>
2023-04-24 16:51:12 -0400

    base de datos actualizada

--- bug_tracking/database/admin.py
+++ bug_tracking/database/admin.py
@@ -2,11 +2,27 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 
-
 admin.site.register(Usuario)
-admin.site.register(Programador)
+
+@admin.register(Programador)
+class ReporteBugAdmin(admin.ModelAdmin):
+    list_display = ('nombre_programador', 'correo_programador')
+    #exclude      = ('nombre_programador', 'correo_programador')
+
 admin.site.register(Proyecto)
-admin.site.register(ReporteBug)
-admin.site.register(Prioridad)
-admin.site.register(Estado)
-admin.site.register(Bug)
+
+@admin.register(Cargo)
+class ReporteBugAdmin(admin.ModelAdmin):
+    list_display = ('programador', 'proyecto')
+    #exclude      = ('nombre_programador', 'correo_programador')
+
+@admin.register(ReporteBug)
+class ReporteBugAdmin(admin.ModelAdmin):
+    list_display = ('titulo','fecha_reporte','bug')
+    #exclude      = ('id_reporte','titulo','reporte','fecha_reporte','usuario')
+
+@admin.register(Bug)
+class ReporteBugAdmin(admin.ModelAdmin):
+    list_display = ('id_bug', 'descripcion', 'proyecto', 'programador', 'estado', 'prioridad')
+    search_fields = ('proyecto', 'programador')
+    #exclude      = ('id_bug', 'descripcion', 'id_proyecto', 'id_programador')
--- bug_tracking/database/models.py
+++ bug_tracking/database/models.py
@@ -5,65 +5,71 @@ from django.db import models
 
 class Usuario(models.Model):
     correo_usuario = models.EmailField(primary_key=True)
+    
+    def __str__(self):
+        return self.correo_usuario
 
     
-
-
 class Programador(models.Model):
-    id_programador = models.AutoField(primary_key=True)
+    id_programador     = models.AutoField(primary_key=True)
     correo_programador = models.EmailField(null=False)
-    nombre_programador = models.CharField(max_length=255, verbose_name='Nombre del programador')
+    nombre_programador = models.CharField(max_length=255,null=False, verbose_name='Nombre del programador')
     
-
-
-
+    def __str__(self):
+        return self.nombre_programador
+    
 
 class Proyecto(models.Model):
-    id_proyecto = models.AutoField(primary_key=True)
+    id_proyecto     = models.AutoField(primary_key=True)
     nombre_proyecto = models.CharField(max_length=255, null=False, verbose_name='Nombre del proyecto')
+    
+    programadores   = models.ManyToManyField(Programador, through="Cargo")
+
+    def __str__(self):
+        return self.nombre_proyecto
+
+class Cargo(models.Model):
+    programador = models.ForeignKey(Programador, null=False, on_delete=models.CASCADE)
+    proyecto    = models.ForeignKey(Proyecto, null=False, on_delete=models.CASCADE)
+    
+    cargo          = models.CharField(max_length=255, null=False, verbose_name='Cargo en el proyecto')
 
 
-
-
-class ReporteBug(models.Model):
-    id_reporte = models.AutoField(primary_key=True)
-    reporte = models.TextField()
-    fecha_reporte = models.DateTimeField(auto_now_add=True, verbose_name='Fecha de reporte')
-    usuario = models.ForeignKey(Usuario, null=False, on_delete=models.CASCADE, related_name='reportes')
-
-
-
-
-class Prioridad(models.Model):
+class Bug(models.Model):
     PRIORIDADES_CHOICES = (
         ("baja", "Baja"),
         ("media", "Media"),
         ("alta", "Alta"),
         ("urgente", "Urgente"),
     )
-    id_prioridad = models.AutoField(primary_key=True)
-    prioridad = models.CharField(max_length=20, choices=PRIORIDADES_CHOICES, verbose_name='Prioridad')
-
- 
-
-class Estado(models.Model):
+    
     ESTADOS_CHOICES = (
         ('nuevo', 'Nuevo'),
         ('en proceso', 'En proceso'),
         ('solucionado', 'Solucionado'),
     )
-
-    id_estado = models.AutoField(primary_key=True)
-    estado = models.CharField(max_length=20, choices=ESTADOS_CHOICES, verbose_name='Estado')
-
-
-
-class Bug(models.Model):
-    id_bug = models.AutoField(primary_key=True)
-    descripcion = models.TextField(verbose_name='Descripción')
-    prioridad = models.ForeignKey(Prioridad,on_delete=models.CASCADE, verbose_name='Prioridad')
-    estado = models.ForeignKey(Estado, null=False, on_delete=models.CASCADE, verbose_name='Estado')
-    proyecto = models.ForeignKey(Proyecto, null=False, on_delete=models.CASCADE, verbose_name='Proyecto')
+    
+    id_bug      = models.AutoField(primary_key=True)
+    descripcion = models.TextField(null=False, verbose_name='Descripción')
+    prioridad   = models.CharField(max_length=20, choices=PRIORIDADES_CHOICES, verbose_name='Prioridad')
+    estado      = models.CharField(max_length=20, choices=ESTADOS_CHOICES, verbose_name='Estado')
+    
+    proyecto    = models.ForeignKey(Proyecto, null=False, on_delete=models.CASCADE, verbose_name='Proyecto')
     programador = models.ForeignKey(Programador, null=False, on_delete=models.CASCADE, verbose_name='Programador')
-    #reportes = models.ManyToManyField(ReporteBug, null=False, related_name='bugs', verbose_name='Reportes')
+    
+    def __str__(self):
+        return str(self.id_bug)
 
+    
+class ReporteBug(models.Model):
+    id_reporte     = models.AutoField(primary_key=True)
+    titulo         = models.CharField(max_length=255, null=False, verbose_name='titulo del reporte')
+    reporte        = models.TextField(null=False)
+    fecha_reporte  = models.DateTimeField(auto_now_add=True, verbose_name='Fecha de reporte')
+    
+    correo_usuario = models.ForeignKey(Usuario, null=False, on_delete=models.CASCADE, related_name='reportes')
+    bug         = models.ForeignKey(Bug, blank=True, null=True, on_delete=models.CASCADE, verbose_name='Bug')
+
+    def __str__(self):
+        return self.titulo
+    
