42e07a6f9dd4ad7d73e702f765236b4b50736808
miosv <mvilla2019@inf.udec.cl>
2023-04-25 18:17:39 -0400

    Arreglo de bugs
    Se arregló un bug que no se podian enviar reportes, y luego un bug que no se podian enviar reportes con correos antiguos.

--- bug_tracking/home/forms.py
+++ bug_tracking/home/forms.py
@@ -6,15 +6,14 @@ class UsuarioForm(forms.ModelForm):
         model = Usuario
         fields = ['correo_usuario']
         widgets = {
-            'correo_usuario': forms.Textarea(attrs={'rows': 1, 'placeholder': 'Introduzca su correo'})
+            'correo_usuario': forms.EmailInput(attrs={'placeholder': 'Introduzca su correo'})
         }
-
 
 class ReporteBugForm(forms.ModelForm):
     class Meta:
         model = ReporteBug
-        fields = ['reporte', 'correo_usuario']
+        fields = ['reporte']
         widgets = {
             'reporte': forms.Textarea(attrs={'rows': 3, 'placeholder': 'Describe el problema aquí...'}),
-            'correo_usuario': forms.Textarea(attrs={'rows': 1, 'placeholder': 'Introduzca su correo'})
         }
+
