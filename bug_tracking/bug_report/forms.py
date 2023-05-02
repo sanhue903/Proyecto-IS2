9bc974c66153d5b0beca2cb1ae9c37669c858483
romizurita <rzurita2018@udec.cl>
2023-05-01 23:28:53 -0400

    Actualizacion inicio

--- bug_tracking/bug_report/forms.py
+++ bug_tracking/bug_report/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from database.models import ReporteBug, Usuario, Proyecto
+from database.models import ReporteBug, Usuario
 
 class UsuarioForm(forms.ModelForm):
     class Meta:
@@ -21,17 +21,4 @@ class ReporteBugForm(forms.ModelForm):
                 'placeholder': 'Describe el problema aquí...',
                 }),
         }
-
-class ProyectoForm(forms.ModelForm):
-    nombre_proyecto = forms.ModelChoiceField(
-        queryset=Proyecto.objects.all(),
-        empty_label=None,
-        widget=forms.Select(attrs={'class': 'form-control'}),
-        to_field_name='id_proyecto',
-        label='Nombre del proyecto'
-    )
-
-    class Meta:
-        model = Proyecto
-        fields = ['nombre_proyecto']
 
--- bug_tracking/home/views.py
+++ bug_tracking/home/views.py
@@ -1,13 +1,31 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.template import loader
 from django.contrib import messages
+from .models import ReporteBug, Bug
+from database.models import ReporteBug, Bug
 # Create your views here.
 
+
 def home(request):
-    return render(request,'home/base.html')
+    listar_reportes = ReporteBug.objects.order_by("id_reporte")[:20]
+
+    listar_bug = Bug.objects.order_by("id_bug")[:20]
+
+    context = {
+        "listar_reportes": listar_reportes,
+        "listar_bug": listar_bug
+    }
+    return render(request, 'home/base.html', context)
 
 
+# def home_inicio(request):
+#     listar_reportes = ReporteBug.objects.order_by("id_reporte")[:20]
 
+#     listar_bug = Bug.objects.order_by("id_bug")[:20]
 
-
+#     context = {
+#         "listar_reportes": listar_reportes,
+#         "listar_bug": listar_bug
+#     }
+#     return render(request, "home/index.html", context)
