26e0887d2f7400f047f21b114405cdb0ba737b8e
miosv <mvilla2019@inf.udec.cl>
2023-05-03 00:34:45 -0400

    Arreglo reporte de bug
    Se arreglo el codigo eliminado de reporte de bug

--- bug_tracking/bug_report/views.py
+++ bug_tracking/bug_report/views.py
@@ -1,24 +1,28 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.template import loader
-from bug_report.forms import ReporteBugForm, UsuarioForm
-from database.models import ReporteBug, Usuario
+from bug_report.forms import ReporteBugForm, UsuarioForm, ProyectoForm
+from database.models import ReporteBug, Usuario, Proyecto
 
 # Create your views here.
 
 def reportar_bug(request):
     form_bug = ReporteBugForm()
     form_usuario = UsuarioForm()
+    form_proyecto = ProyectoForm()
     if request.method == 'POST':
         form_bug = ReporteBugForm(request.POST)
+        form_proyecto = ProyectoForm(request.POST)
         correo_usuario = request.POST.get('correo_usuario')
         try:
             usuario = Usuario.objects.get(correo_usuario=correo_usuario)
         except Usuario.DoesNotExist:
             usuario = None
-        
-        if form_bug.is_valid():
+        id_proyecto = request.POST.get('nombre_proyecto')
+        id_proyecto2 = Proyecto.objects.get(id_proyecto=id_proyecto)
+        if form_bug.is_valid() and form_proyecto.is_valid():
             reporte_bug = form_bug.save(commit=False)
+            # reporte_proyecto = form_proyecto.save(commit=False)
             if usuario:
                 reporte_bug.correo_usuario = usuario
             else:
@@ -27,13 +31,17 @@ def reportar_bug(request):
                     usuario = form_usuario.save()
                     reporte_bug.correo_usuario = usuario
                 else:
-                    return render(request, 'bug_report/reporte_bug.html', {'form_bug': form_bug, 'form_usuario': form_usuario})
+                    return render(request, 'bug_report/reporte_bug.html', {'form_bug': form_bug, 'form_usuario': form_usuario, 'form_proyecto': form_proyecto})
+            
+            # idProyecto = reporte_proyecto.save()
+            reporte_bug.id_proyecto = id_proyecto2
             reporte_bug.save()
             return redirect('report:confirmacion_reporte')
     else:
         form_bug = ReporteBugForm()
         form_usuario = UsuarioForm()
-    return render(request, 'bug_report/reporte_bug.html', {'form_bug': form_bug, 'form_usuario': form_usuario})
+        form_proyecto = ProyectoForm()
+    return render(request, 'bug_report/reporte_bug.html', {'form_bug': form_bug, 'form_usuario': form_usuario, 'form_proyecto': form_proyecto})
 
 def confirmacion_reporte(request):
     return render(request, 'bug_report/confirmacion_reporte.html')
