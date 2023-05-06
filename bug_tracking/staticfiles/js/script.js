a8445665f2472fd2b91e366680c7bef7d5a978a8
Yerko <bryanreyesavila@outlook.cl>
2023-05-05 21:49:54 -0400

    Sidebar responsivo

--- bug_tracking/staticfiles/js/script.js
+++ bug_tracking/staticfiles/js/script.js
@@ -1,34 +1,80 @@
-const body = document.querySelector('body');
-const sidebar = body.querySelector('nav');
-const toggle = body.querySelector(".toggle");
-const modeSwitch = body.querySelector(".toggle-switch");
-const modeText = body.querySelector(".mode-text");
+function onload() {
+  const body = document.querySelector("body");
+  const sidebar = body.querySelector("nav");
+  const toggle = body.querySelector(".toggle");
+  const modeSwitch = body.querySelector(".toggle-switch");
+  const modeText = body.querySelector(".mode-text");
+  const home = document.querySelector("#home .toggle");
+  const touch = body.querySelector("home");
 
-const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+  const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
 
-if(body.offsetWidth <= 1024){
+  /* if (prefersDarkScheme.matches) {
+        document.body.classList.toggle("dark", localStorage.getItem('darkmode') === 'true');
+        modeText.innerText = "Modo claro";
+    } */
+
+  toggle.addEventListener("click", () => {
     sidebar.classList.toggle("close");
-}
-if (prefersDarkScheme.matches) {
-    document.body.classList.toggle("dark", localStorage.getItem('darkmode') === 'true');
-    modeText.innerText = "Modo claro";
-}
+  });
 
+    home.addEventListener("click", () => {
+    
+    sidebar.classList.toggle("unhide");
+  });
 
-toggle.addEventListener("click" , () =>{
-    sidebar.classList.toggle("close");
-})
-
-modeSwitch.addEventListener("click" , () =>{
-
-    const WasDarkMode = localStorage.getItem('darkmode') === 'true'
-    localStorage.setItem('darkmode', !WasDarkMode);
+  modeSwitch.addEventListener("click", () => {
+    const WasDarkMode = localStorage.getItem("darkmode") === "true";
+    localStorage.setItem("darkmode", !WasDarkMode);
     const element = document.body;
     element.classList.toggle("dark");
+  });
 
-});
+  document.body.classList.toggle(
+    "dark",
+    localStorage.getItem("darkmode") === "true"
+  );
 
-function onload(){
-    document.body.classList.toggle("dark", localStorage.getItem('darkmode') === 'true');
+  window.addEventListener("load", () => {
+    home.style.display = "none";
+    if (screen.width > 850) {
+      sidebar.classList.remove("close");
+    } else {
+      if (screen.width < 850) {
+        sidebar.classList.add("close");
+        console.log("entre al load");
+      }
+      if (screen.width < 600) {
+        console.log("entre al load");
+        sidebar.classList.add("close");
+        sidebar.classList.add("hide");
+        home.style.display = "block";
+      } else {
+        sidebar.classList.remove("hide");
+      }
+    }
+  });
+
+  console.log(screen.width);
+  window.addEventListener("resize", () => {
+    home.style.display = "none";
+    if (screen.width > 850) {
+      sidebar.classList.remove("close");
+    } else {
+      if (screen.width < 850) {
+        sidebar.classList.add("close");
+
+        if (screen.width < 600) {
+          console.log("entre al resize");
+          sidebar.classList.add("close");
+          sidebar.classList.add("hide");
+          home.style.display = "block";
+        } else {
+          sidebar.classList.remove("hide");
+        }
+      }
+    }
+  });
 }
 
+document.addEventListener("DOMContentLoaded", onload);
