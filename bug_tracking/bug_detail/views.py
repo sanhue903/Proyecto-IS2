a616bb502f67479f26fbbe84c8f3efe6cc02df5a
miosv <mvilla2019@inf.udec.cl>
2023-05-17 20:15:21 -0400

    se agregaron los detalles de un bug

--- bug_tracking/bug_detail/views.py
+++ bug_tracking/bug_detail/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
 
-from database.models import Bug
+from database.models import Bug, Proyecto, ReporteBug, Programador
 
 # Create your views here.
 
 def index(request, bug_id):
     bug = Bug.objects.get(id_bug = bug_id)
-    context = {"bug": bug}
+    proyecto = Proyecto.objects.get(id_proyecto = bug.id_proyecto.id_proyecto)
+    programador = Programador.objects.get(id_programador = bug.id_programador.id_programador)
+    reporte = ReporteBug.objects.order_by("id_reporte").filter(id_bug_id = bug_id)[:20]
+
+
+    context = {"bug": bug, "proyecto": proyecto, "programador": programador, "reporte": reporte}
     return render(request, "bug_detail/index.html", context)
