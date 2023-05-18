a57dd18d7fe415df570ad1f99760c8b40ca1a4f7
Daniel Michel <timydan@gmail.com>
2023-05-18 18:56:48 -0400

    migración de base de datos a postgreSQL

--- bug_tracking/bug_tracking/settings.py
+++ bug_tracking/bug_tracking/settings.py
@@ -80,8 +80,13 @@ WSGI_APPLICATION = 'bug_tracking.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        # BASE_DIR / 
+        'NAME': 'is2',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
