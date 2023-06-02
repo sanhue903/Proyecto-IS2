2693706baa6c4feedc249bbaf1b2a84765eeb9ee
miosv <mvilla2019@inf.udec.cl>
2023-06-02 14:50:01 -0400

    Cambios en buglist, se arreglo el estado, el proyecto y se agrego para poder limitar el titulo

--- bug_tracking/bug_detail/views.py
+++ bug_tracking/bug_detail/views.py
@@ -10,12 +10,20 @@ def index(request, bug_id):
     bug = Bug.objects.get(id_bug = bug_id)
     proyecto = Proyecto.objects.get(id_proyecto = bug.id_proyecto.id_proyecto)
     programador = bug.id_programador
-    usuario = User.objects.get(id = programador.user_id)
+    usuario_programador = User.objects.get(id = programador.user_id)
     avance = Avances.objects.order_by("-id_avance").filter(id_bug_id = bug_id)
 
     reporte = ReporteBug.objects.order_by("-id_reporte").filter(id_bug_id = bug_id)
+    usuarios_reporte = User.objects.filter(id__in=reporte.values_list("id_usuario", flat=True))
+
     imagen = Imagen.objects.order_by("id_imagen").filter(id_reporte_id__in = reporte)
 
-
-    context = {"bug": bug, "proyecto": proyecto, "programador": usuario, "avance": avance, "imagen":imagen, "reporte":reporte}
+    context = {"bug": bug, 
+               "proyecto": proyecto,
+                "programador": usuario_programador, 
+                "avance": avance, 
+                "imagen":imagen, 
+                "reporte": zip(reporte, usuarios_reporte)
+                }
+    
     return render(request, "bug_detail/index.html", context)
--- bug_tracking/buglist/views.py
+++ bug_tracking/buglist/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
-from database.models import Bug, ReporteBug
+from database.models import Bug, ReporteBug, Proyecto
 
 
 
 # Create your views here.
 
 def index(request):
-    bug_list = Bug.objects.order_by("id_bug")[:20]
-    report_list = ReporteBug.objects.order_by("id_reporte")[:20]
-    context = {"bug_list": bug_list, "report_list": report_list}
+    bug_list = Bug.objects.order_by("-fecha_reporte")
+    report_list = ReporteBug.objects.order_by("-fecha_reporte")
+    proyecto = Proyecto.objects.filter(id_proyecto__in=report_list.values_list("id_proyecto", flat=True))
+
+    context = {"bug_list": bug_list, "report_list": zip(report_list, proyecto)}
     return render(request, "buglist/buglist.html", context)
 
 
