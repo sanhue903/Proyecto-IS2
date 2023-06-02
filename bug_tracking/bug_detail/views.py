4e72d25897234fb7a2b42bb5dcecff656a68d444
miosv <mvilla2019@inf.udec.cl>
2023-06-02 04:35:23 -0400

    cambio en el orden de los  tickets y reportes (del ultimo al primero)

--- bug_tracking/bug_detail/views.py
+++ bug_tracking/bug_detail/views.py
@@ -11,9 +11,9 @@ def index(request, bug_id):
     proyecto = Proyecto.objects.get(id_proyecto = bug.id_proyecto.id_proyecto)
     programador = bug.id_programador
     usuario = User.objects.get(id = programador.user_id)
-    avance = Avances.objects.order_by("id_avance").filter(id_bug_id = bug_id)
+    avance = Avances.objects.order_by("-id_avance").filter(id_bug_id = bug_id)
 
-    reporte = ReporteBug.objects.order_by("id_reporte").filter(id_bug_id = bug_id)
+    reporte = ReporteBug.objects.order_by("-id_reporte").filter(id_bug_id = bug_id)
     imagen = Imagen.objects.order_by("id_imagen").filter(id_reporte_id__in = reporte)
 
 
