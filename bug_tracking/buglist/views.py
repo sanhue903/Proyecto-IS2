d97bf729a3ef864f6e2be9812f75836bfbc5478c
miosv <mvilla2019@inf.udec.cl>
2023-06-02 15:01:55 -0400

    Arregle un bug que no mostraba bien los proyecto en los reportes de buglist

--- bug_tracking/buglist/views.py
+++ bug_tracking/buglist/views.py
@@ -8,10 +8,9 @@ from database.models import Bug, ReporteBug, Proyecto
 
 def index(request):
     bug_list = Bug.objects.order_by("-fecha_reporte")
-    report_list = ReporteBug.objects.order_by("-fecha_reporte")
-    proyecto = Proyecto.objects.filter(id_proyecto__in=report_list.values_list("id_proyecto", flat=True))
+    report_list = ReporteBug.objects.order_by("-fecha_reporte").select_related("id_proyecto")
 
-    context = {"bug_list": bug_list, "report_list": zip(report_list, proyecto)}
+    context = {"bug_list": bug_list, "report_list": report_list}
     return render(request, "buglist/buglist.html", context)
 
 
