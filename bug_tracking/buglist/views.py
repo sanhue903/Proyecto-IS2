3d6453820a573c7bd15c908d3ce0fa9608c83c9f
miosv <mvilla2019@inf.udec.cl>
2023-06-09 23:25:27 -0400

    Se creo la paginacion en buglist con su estilizacion

--- bug_tracking/buglist/views.py
+++ bug_tracking/buglist/views.py
@@ -1,18 +1,24 @@
 from django.shortcuts import render
-from django.shortcuts import redirect
+from django.core.paginator import Paginator
 from database.models import Bug, ReporteBug, Proyecto
-
-
-
-# Create your views here.
 
 def index(request):
     bug_list = Bug.objects.order_by("-fecha_reporte")
+    bug_paginator = Paginator(bug_list, 5)  # Paginación para la lista de bugs
+
     report_list = ReporteBug.objects.order_by("-fecha_reporte").select_related("id_proyecto")
+    report_paginator = Paginator(report_list, 5)  # Paginación para los reportes
 
-    context = {"bug_list": bug_list, "report_list": report_list}
+    bug_page_number = request.GET.get('bug_page')
+    bug_page_obj = bug_paginator.get_page(bug_page_number)
+    bug_page_obj.adjusted_elided_pages = bug_paginator.get_elided_page_range(bug_page_obj.number, on_each_side=1, on_ends=1)
+
+    report_page_number = request.GET.get('report_page')
+    report_page_obj = report_paginator.get_page(report_page_number)
+    report_page_obj.adjusted_elided_pages = report_paginator.get_elided_page_range(report_page_obj.number, on_each_side=1, on_ends=1)
+
+    context = {
+        "bug_page_obj": bug_page_obj,
+        "report_page_obj": report_page_obj
+    }
     return render(request, "buglist/buglist.html", context)
-
-
-def refresh(request):
-    return redirect('buglist:index')
