8d03b76051217f4a4a657a3c6cd7202061230b03
miosv <mvilla2019@inf.udec.cl>
2023-06-19 02:24:46 -0400

    Tengo un filtro de asc y des en las dos tablas de buglist, pero los filtros se aplicar al mismo tiempo a las tablas

--- bug_tracking/buglist/urls.py
+++ bug_tracking/buglist/urls.py
@@ -4,8 +4,7 @@ from . import views
 app_name = "buglist"
 urlpatterns = [
     path('', views.index, name='bug_list'),
-    # Ruta para la paginación de la lista de bugs
     path('bugs/', views.index, name='bug_list_pagination'),
-    # Ruta para la paginación de los reportes
+    path('bugs/<str:order>/', views.index, name='bug_list_order'),
     path('reports/', views.index, name='report_list_pagination'),
 ]
--- bug_tracking/buglist/views.py
+++ bug_tracking/buglist/views.py
@@ -2,11 +2,65 @@ from django.shortcuts import render
 from django.core.paginator import Paginator
 from database.models import Bug, ReporteBug, Proyecto
 
-def index(request):
-    bug_list = Bug.objects.order_by("-fecha_reporte")
-    bug_paginator = Paginator(bug_list, 5)  # Paginación para la lista de bugs
+def index(request, order=''):  # Agregamos el parámetro de ordenamiento con un valor predeterminado vacío
 
-    report_list = ReporteBug.objects.order_by("-fecha_reporte").select_related("id_proyecto")
+    # Obtener el parámetro de dirección del ordenamiento
+    order_direction = request.GET.get('order')
+
+    # Ordenar la lista de bugs por ID
+    if order == 'id':
+        if order_direction == 'asc':
+            bug_list = Bug.objects.order_by("id_bug")
+        else:
+            bug_list = Bug.objects.order_by("-id_bug")
+    elif order == 'titulo':
+        if order_direction == 'asc':
+            bug_list = Bug.objects.order_by("titulo")
+        else:
+            bug_list = Bug.objects.order_by("-titulo")
+    elif order == 'fecha':
+        if order_direction == 'asc':
+            bug_list = Bug.objects.order_by("fecha_reporte")
+        else:
+            bug_list = Bug.objects.order_by("-fecha_reporte")
+    elif order == 'estado':
+        if order_direction == 'asc':
+            bug_list = Bug.objects.order_by("estado")
+        else:
+            bug_list = Bug.objects.order_by("-estado") 
+    elif order == 'proyecto':
+        if order_direction == 'asc':
+            bug_list = Bug.objects.order_by("id_proyecto__nombre_proyecto")
+        else:
+            bug_list = Bug.objects.order_by("-id_proyecto__nombre_proyecto")
+    else:
+        bug_list = Bug.objects.order_by("-fecha_reporte")
+    
+    # Ordenar la lista de reportes por ID
+    if order == 'id':
+        if order_direction == 'asc':
+            report_list = ReporteBug.objects.order_by("id_reporte")
+        else:
+            report_list = ReporteBug.objects.order_by("-id_reporte")
+    elif order == 'titulo':
+        if order_direction == 'asc':
+            report_list = ReporteBug.objects.order_by("titulo")
+        else:
+            report_list = ReporteBug.objects.order_by("-titulo")
+    elif order == 'fecha':
+        if order_direction == 'asc':
+            report_list = ReporteBug.objects.order_by("fecha_reporte")
+        else:
+            report_list = ReporteBug.objects.order_by("-fecha_reporte")
+    elif order == 'proyecto':
+        if order_direction == 'asc':
+            report_list = ReporteBug.objects.order_by("id_proyecto__nombre_proyecto")
+        else:
+            report_list = ReporteBug.objects.order_by("-id_proyecto__nombre_proyecto")
+    else:
+        report_list = ReporteBug.objects.order_by("-fecha_reporte").select_related("id_proyecto")
+
+    bug_paginator = Paginator(bug_list, 5)  # Paginación para la lista de bugs
     report_paginator = Paginator(report_list, 5)  # Paginación para los reportes
 
     bug_page_number = request.GET.get('bug_page')
@@ -19,6 +73,7 @@ def index(request):
 
     context = {
         "bug_page_obj": bug_page_obj,
-        "report_page_obj": report_page_obj
+        "report_page_obj": report_page_obj,
+        "order": order  # Agregamos la variable de ordenamiento al contexto
     }
     return render(request, "buglist/buglist.html", context)
