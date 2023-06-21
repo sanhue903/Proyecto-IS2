79b3f264f441ff7c52c73d598a5af0e2ff50c516
miosv <mvilla2019@inf.udec.cl>
2023-06-21 19:00:51 -0400

    Se arreglo el bug de que cuando se aplica un filtro se aplicaba a las dos tablas en vez de cada una por separado

--- bug_tracking/buglist/urls.py
+++ bug_tracking/buglist/urls.py
@@ -5,6 +5,7 @@ app_name = "buglist"
 urlpatterns = [
     path('', views.index, name='bug_list'),
     path('bugs/', views.index, name='bug_list_pagination'),
-    path('bugs/<str:order>/', views.index, name='bug_list_order'),
+    path('bugs/<str:bug_order>/', views.index, name='bug_list_order'),
     path('reports/', views.index, name='report_list_pagination'),
+    path('reports/<str:report_order>/', views.index, name='report_list_order'),
 ]
--- bug_tracking/buglist/views.py
+++ bug_tracking/buglist/views.py
@@ -2,78 +2,82 @@ from django.shortcuts import render
 from django.core.paginator import Paginator
 from database.models import Bug, ReporteBug, Proyecto
 
-def index(request, order=''):  # Agregamos el parámetro de ordenamiento con un valor predeterminado vacío
+def index(request):
 
-    # Obtener el parámetro de dirección del ordenamiento
-    order_direction = request.GET.get('order')
+    bug_order = request.GET.get('bug_order', '')
+    report_order = request.GET.get('report_order', '')
+    bug_order_direction = request.GET.get('bug_order_direction', '')
+    report_order_direction = request.GET.get('report_order_direction', '')
 
-    # Ordenar la lista de bugs por ID
-    if order == 'id':
-        if order_direction == 'asc':
-            bug_list = Bug.objects.order_by("id_bug")
-        else:
-            bug_list = Bug.objects.order_by("-id_bug")
-    elif order == 'titulo':
-        if order_direction == 'asc':
-            bug_list = Bug.objects.order_by("titulo")
-        else:
-            bug_list = Bug.objects.order_by("-titulo")
-    elif order == 'fecha':
-        if order_direction == 'asc':
-            bug_list = Bug.objects.order_by("fecha_reporte")
-        else:
-            bug_list = Bug.objects.order_by("-fecha_reporte")
-    elif order == 'estado':
-        if order_direction == 'asc':
-            bug_list = Bug.objects.order_by("estado")
-        else:
-            bug_list = Bug.objects.order_by("-estado") 
-    elif order == 'proyecto':
-        if order_direction == 'asc':
-            bug_list = Bug.objects.order_by("id_proyecto__nombre_proyecto")
-        else:
-            bug_list = Bug.objects.order_by("-id_proyecto__nombre_proyecto")
-    else:
-        bug_list = Bug.objects.order_by("-fecha_reporte")
-    
-    # Ordenar la lista de reportes por ID
-    if order == 'id':
-        if order_direction == 'asc':
-            report_list = ReporteBug.objects.order_by("id_reporte")
-        else:
-            report_list = ReporteBug.objects.order_by("-id_reporte")
-    elif order == 'titulo':
-        if order_direction == 'asc':
-            report_list = ReporteBug.objects.order_by("titulo")
-        else:
-            report_list = ReporteBug.objects.order_by("-titulo")
-    elif order == 'fecha':
-        if order_direction == 'asc':
-            report_list = ReporteBug.objects.order_by("fecha_reporte")
-        else:
-            report_list = ReporteBug.objects.order_by("-fecha_reporte")
-    elif order == 'proyecto':
-        if order_direction == 'asc':
-            report_list = ReporteBug.objects.order_by("id_proyecto__nombre_proyecto")
-        else:
-            report_list = ReporteBug.objects.order_by("-id_proyecto__nombre_proyecto")
-    else:
-        report_list = ReporteBug.objects.order_by("-fecha_reporte").select_related("id_proyecto")
+    bug_list = Bug.objects.all()
+    report_list = ReporteBug.objects.all()
 
-    bug_paginator = Paginator(bug_list, 5)  # Paginación para la lista de bugs
-    report_paginator = Paginator(report_list, 5)  # Paginación para los reportes
+    if bug_order == 'id':
+        if bug_order_direction == 'asc':
+            bug_list = bug_list.order_by("id_bug")
+        else:
+            bug_list = bug_list.order_by("-id_bug")
+    elif bug_order == 'titulo':
+        if bug_order_direction == 'asc':
+            bug_list = bug_list.order_by("titulo")
+        else:
+            bug_list = bug_list.order_by("-titulo")
+    elif bug_order == 'fecha':
+        if bug_order_direction == 'asc':
+            bug_list = bug_list.order_by("fecha_reporte")
+        else:
+            bug_list = bug_list.order_by("-fecha_reporte")
+    elif bug_order == 'estado':
+        if bug_order_direction == 'asc':
+            bug_list = bug_list.order_by("estado")
+        else:
+            bug_list = bug_list.order_by("-estado")
+    elif bug_order == 'proyecto':
+        if bug_order_direction == 'asc':
+            bug_list = bug_list.order_by("id_proyecto__nombre_proyecto")
+        else:
+            bug_list = bug_list.order_by("-id_proyecto__nombre_proyecto")
 
+    if report_order == 'id':
+        if report_order_direction == 'asc':
+            report_list = report_list.order_by("id_reporte")
+        else:
+            report_list = report_list.order_by("-id_reporte")
+    elif report_order == 'titulo':
+        if report_order_direction == 'asc':
+            report_list = report_list.order_by("titulo")
+        else:
+            report_list = report_list.order_by("-titulo")
+    elif report_order == 'fecha':
+        if report_order_direction == 'asc':
+            report_list = report_list.order_by("fecha_reporte")
+        else:
+            report_list = report_list.order_by("-fecha_reporte")
+    elif report_order == 'estado':
+        if report_order_direction == 'asc':
+            report_list = report_list.order_by("estado")
+        else:
+            report_list = report_list.order_by("-estado")
+    elif report_order == 'proyecto':
+        if report_order_direction == 'asc':
+            report_list = report_list.order_by("id_proyecto__nombre_proyecto")
+        else:
+            report_list = report_list.order_by("-id_proyecto__nombre_proyecto")
+
+    bug_paginator = Paginator(bug_list, 5)
     bug_page_number = request.GET.get('bug_page')
     bug_page_obj = bug_paginator.get_page(bug_page_number)
     bug_page_obj.adjusted_elided_pages = bug_paginator.get_elided_page_range(bug_page_obj.number, on_each_side=1, on_ends=1)
 
+    report_paginator = Paginator(report_list, 5)
     report_page_number = request.GET.get('report_page')
     report_page_obj = report_paginator.get_page(report_page_number)
     report_page_obj.adjusted_elided_pages = report_paginator.get_elided_page_range(report_page_obj.number, on_each_side=1, on_ends=1)
 
     context = {
-        "bug_page_obj": bug_page_obj,
-        "report_page_obj": report_page_obj,
-        "order": order  # Agregamos la variable de ordenamiento al contexto
+        'bug_page_obj': bug_page_obj,
+        'report_page_obj': report_page_obj,
+        'bug_order': bug_order,
+        'report_order': report_order,
     }
-    return render(request, "buglist/buglist.html", context)
+    return render(request, 'buglist/buglist.html', context)
