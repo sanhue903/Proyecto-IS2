b851f0b91c06b23be22de3fab47fbfc929e9a3dd
Yerko <bryanreyesavila@outlook.cl>
2023-06-21 14:48:57 -0400

    Correción de errores

--- bug_tracking/home/views.py
+++ bug_tracking/home/views.py
@@ -12,24 +12,24 @@ from .forms import CustomUserCreationForm
 
 def home(request):
 
-    if(request.user.is_authenticated == True):
-        """ reportes_usuario = ReporteBug.objects.filter(id_usuario=request.user.id).order_by("-fecha_reporte")[:5].select_related("id_proyecto") """
-        usuario = Usuario.objects.get(id_user=request.user)
-        reportes_usuario = ReporteBug.objects.filter(id_usuario = usuario)
+    if(request.user.is_authenticated == True and request.user.is_superuser == False):
         
+            usuario = Usuario.objects.get(id_user=request.user)
+            reportes_usuario = ReporteBug.objects.filter(id_usuario = usuario)
+            
 
-        reportes_usario_id = reportes_usuario.values_list('id_reporte', flat=True)
-        reportes_solucionados = Bug.objects.filter(id_bug__in=reportes_usario_id, estado="SOLUCIONADO").count()
+            reportes_usario_id = reportes_usuario.values_list('id_reporte', flat=True)
+            reportes_solucionados = Bug.objects.filter(id_bug__in=reportes_usario_id, estado="SOLUCIONADO").count()
 
-        reportes_asignados= Bug.objects.filter(id_bug__in=reportes_usario_id, estado="ASIGNADO").count()
-        reportes_enproceso= Bug.objects.filter(id_bug__in=reportes_usario_id, estado="EN PROCESO").count()
-        context = {
-            "reportes_usuario": reportes_usuario,
-            "reportes_solucionados": reportes_solucionados,
-            "reportes_asignados": reportes_asignados,
-            "reportes_enproceso": reportes_enproceso
-        }
-        return render(request,'home/start.html',context)
+            reportes_asignados= Bug.objects.filter(id_bug__in=reportes_usario_id, estado="ASIGNADO").count()
+            reportes_enproceso= Bug.objects.filter(id_bug__in=reportes_usario_id, estado="EN PROCESO").count()
+            context = {
+                "reportes_usuario": reportes_usuario,
+                "reportes_solucionados": reportes_solucionados,
+                "reportes_asignados": reportes_asignados,
+                "reportes_enproceso": reportes_enproceso
+            }
+            return render(request,'home/start.html',context)
     else:
         listar_reportes = ReporteBug.objects.order_by("-fecha_reporte")[:5].select_related("id_proyecto")
         total_report = ReporteBug.objects.count()
