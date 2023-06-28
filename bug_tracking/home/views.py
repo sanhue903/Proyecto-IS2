e18235a5e231a95d32ea9caf8a10bc05e8a289c4
Yerko <bryanreyesavila@outlook.cl>
2023-06-28 10:47:56 -0400

    Correción

--- bug_tracking/home/views.py
+++ bug_tracking/home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+import plotly
 from .models import ReporteBug, Bug
 from database.models import ReporteBug, Bug, Usuario, Notificaciones
 from django.contrib.auth.decorators import login_required
@@ -42,7 +43,7 @@ def home(request):
                 else:
                     fig = px.bar(df, x='Titulo', y='Proyecto', width=1000)
                     fig.update_layout(paper_bgcolor='rgba(0,0,0,0)',plot_bgcolor='rgba(0,0,0,0)',font=dict(family='Segoe UI', color='white'))
-                    gantt_plot = plot(fig, output_type='div')
+                    gantt_plot = plotly(fig, output_type='div')
                     context = {
                         "bugs_asignados": bugs_asignados,
                         "gantt_plot": gantt_plot
@@ -72,8 +73,6 @@ def home(request):
         else:
             reportes_proyectos = ReporteBug.objects.filter(fecha_reporte__gte=timezone.now() - timedelta(days=7)).select_related('id_proyecto')
             proyectos = reportes_proyectos.values_list('id_proyecto__nombre_proyecto', flat=True).distinct()
-            """ Es administrador """
-            reportes = reportes = ReporteBug.objects.filter(fecha_reporte__gte=timezone.now()-timedelta(days=7)).values('fecha_reporte__date').annotate(cantidad_reportes=Count('id_reporte'))
             cant_reportes_pendientes = ReporteBug.objects.filter(estado="PENDIENTE").count()
             cant_reportes_asignados = ReporteBug.objects.filter(estado="ASIGNADO").count()
             cant_bugs_revision = Bug.objects.filter(estado="REVISION").count()
@@ -94,10 +93,148 @@ def home(request):
                     'cant_bugs_revision': cant_bugs_revision,
                 }
                 return render(request,'home/start.html', context)
-            fig = px.bar(df, x='fecha', y='cantidad', width=1000)
-            fig.update_layout(paper_bgcolor='rgba(0,0,0,0)',plot_bgcolor='rgba(0,0,0,0)',font=dict(family='Segoe UI', color='white'))
-            """ fig.update_traces(marker=dict(color='yellow', size=10)) """
-            gantt_plot = plot(fig, output_type='div')
+            modo = obtenerDarkMode(request)
+            if modo == 'dark':
+                color_texto = '#707070'
+                color_fondo = '#FFF'
+            else:
+                color_texto = '#ccc'
+                color_fondo = '#242526'
+
+            # Crear el gráfico de barras
+            bar_fig = go.Figure(data=go.Bar(
+                x=df['cantidad_reportes'],
+                y=df['proyecto'],
+                orientation='h',
+            ))
+
+            bar_fig.update_traces(
+                marker_line_width=1.5,
+                #opacity=0.6,
+                showlegend=False,
+            )
+            bar_fig.update_layout(
+                paper_bgcolor=color_fondo,
+                plot_bgcolor=color_fondo,
+                font=dict(family='Segoe UI', color=color_texto),
+                yaxis=dict(
+                    automargin=True,
+                    tickmode='array',
+                    ticktext=[proyecto for proyecto in df['proyecto']],
+                    tickangle=0,
+                    # Dar un espacio en pixeles entre proyectos y el eje y
+                    ticksuffix='  ',
+                ),
+                dragmode=False,
+                title='Reportes por proyecto en los últimos 7 días',
+                yaxis_title='Proyecto',
+                xaxis_title='Cantidad de reportes',
+
+            )
+
+            # Datos para el gráfico de torta
+            cant_reportes_aprobados = ReporteBug.objects.filter(estado="APROBADO").count()
+            cant_reportes_desaprobados = ReporteBug.objects.filter(estado="DESAPROBADO").count()
+            cant_reportes_pendientes = ReporteBug.objects.filter(estado="PENDIENTE").count()
+            labels = ['Aprobados', 'Desaprobados', 'Pendientes']
+            values = [cant_reportes_aprobados, cant_reportes_desaprobados, cant_reportes_pendientes]
+
+            # Crear el gráfico de torta
+            pie_fig1 = go.Figure(data=go.Pie(
+                labels=labels,
+                values=values,
+                hole=0.4,
+                hoverinfo="label+percent",
+                textinfo="value",
+                textfont=dict(size=14),
+                name='Estado de los reportes'
+            ))
+
+            # Personalizar el diseño del gráfico de torta
+            pie_fig1.update_traces(
+                hole=0.4,
+                hoverinfo="label+percent",
+                textinfo="value",
+                textfont=dict(size=14)
+            )
+            pie_fig1.update_layout(
+                paper_bgcolor=color_fondo,
+                plot_bgcolor=color_fondo,
+                font=dict(family='Segoe UI', color='white'),
+                title='Estado de los reportes',
+            )
+
+            # Datos para el gráfico de torta de estados de los bugs
+            cant_bugs_asignados = Bug.objects.filter(estado="ASIGNADO").count()
+            cant_bugs_enproceso = Bug.objects.filter(estado="EN PROCESO").count()
+            cant_bugs_solucionados = Bug.objects.filter(estado="SOLUCIONADO").count()
+            labels_bugs = ['Asignados', 'En proceso', 'Solucionados']
+            values_bugs = [cant_bugs_asignados, cant_bugs_enproceso, cant_bugs_solucionados]
+
+            # Crear el segundo gráfico de torta
+            pie_fig2 = go.Figure(data=go.Pie(
+                labels=labels_bugs,
+                values=values_bugs,
+                hole=0.4,
+                hoverinfo="label+percent",
+                textinfo="value",
+                textfont=dict(size=14),
+                name='Estado de los bugs'
+            ))
+            # Personalizar el diseño del segundo gráfico de torta
+            pie_fig2.update_traces(
+                hole=0.4, 
+                hoverinfo="label+percent", 
+                textinfo="value", 
+                textfont=dict(size=14)
+            )
+            pie_fig2.update_layout(
+                paper_bgcolor='rgba(0,0,0,0)',
+                plot_bgcolor='rgba(0,0,0,0)',
+                font=dict(family='Segoe UI', color=color_texto),
+                title='Estado de los bugs',
+            )
+
+            # Configurar los subplots
+            fig = make_subplots(
+                rows=1, 
+                cols=2, 
+                subplot_titles=(
+                                "Estado de los reportes", 
+                                "Estado de los bugs"
+                                ), 
+                specs=[[{"type": "pie"}, {"type": "pie"}]],
+            )
+            
+            # Agregar el primer gráfico de torta a la subparcela (1, 2) con su propia leyenda
+            fig.add_trace(pie_fig1.data[0], row=1, col=1,)
+
+            # Agregar el segundo gráfico de torta a la subparcela (2, 2) con su propia leyenda
+            fig.add_trace(pie_fig2.data[0], row=1, col=2,)
+
+            fig.update_layout(
+                paper_bgcolor=color_fondo,
+                plot_bgcolor=color_fondo,
+                font=dict(family='Segoe UI', color=color_texto),
+                title_text="Reportes y bugs",
+                height=400,
+                width=1000,
+                dragmode=False,
+
+            )
+
+            fig.update_layout(
+                legend=dict(
+                    yanchor="bottom",
+                    y=0.1,
+                    xanchor="center",
+                    x=0.5
+                )
+            )
+
+            bar_fig = bar_fig.to_html(full_html=False, default_height=300, default_width=1000, config={'displayModeBar': False})
+            plot_div = fig.to_html(full_html=False, default_height=300, default_width=1000, config={'displayModeBar': False})
+
             context = {
                 'cant_reportes_pendientes': cant_reportes_pendientes,
                 'cant_reportes_asignados': cant_reportes_asignados,
